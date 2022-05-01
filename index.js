6122cb5f335a75242bc1e770809829dfbbc15294
Em <ekramm@me.com>
2022-05-01 15:49:31 -0400

    commit tree feed8c1761fc06ed0386864fffcc79512e2a948d

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce(function(accumulator, element) { return element + accumulator}, 0);
+
+// const doubledAndSummed = [1, 2, 3].reduce(function(accumulator, element){ return element * 2 + accumulator}, 0)
